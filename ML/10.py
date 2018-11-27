17950cbdb7a92f25bc0f54e57f9e9d1fd42ee061
rednithin <reddy.nithinpg@live.com>
2018-11-28 01:36:02 +0530

    Made code shorter.

--- ML/10.py
+++ ML/10.py
@@ -7,16 +7,12 @@ n = 100
 x = np.linspace(0, 2 * math.pi, n)
 y = np.sin(x) + 0.3 * np.random.randn(n)
 
-neigh = KNeighborsRegressor(n_neighbors=15)
+neigh = KNeighborsRegressor(n_neighbors=20)
 neigh.fit(x.reshape(-1, 1), y.reshape(-1, 1))
+newY = neigh.predict(x.reshape(-1, 1))
 
-newX = np.linspace(0, 2 * math.pi, n)
-newY = [neigh.predict(np.array([i]).reshape(-1, 1)) for i in newX]
-newY = np.array([a[0][0] for a in newY])
-
-print(newX, newY)
 pl.clf()
 pl.plot(x, y, label='Noisy')
-pl.plot(newX, newY, label='Prediction')
+pl.plot(x, newY, label='Prediction')
 pl.legend(loc='upper right')
 pl.show()
