cdf2bc81f13a1f2fbb5ffd85c51a53be2eb64a76
rednithin <reddy.nithinpg@live.com>
2018-11-29 02:51:24 +0530

    Added silhouette score to 8th

--- ML/8.py
+++ ML/8.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 from sklearn.datasets import load_iris
 from sklearn.cluster import KMeans
 from sklearn.mixture import GaussianMixture
+from sklearn.metrics import silhouette_score
 
 colormap = np.array(['red', 'lime', 'black'])
 
@@ -24,6 +25,9 @@ kmeans.fit(X)
 gmm = GaussianMixture(n_components=3)
 gmm.fit(X)
 gmm_y = gmm.predict(X)
+
+print('Kmeans: ', silhouette_score(X, kmeans.labels_))
+print('GMM: ', silhouette_score(X, gmm_y))
 
 plot(1, 'Real', Y)
 plot(2, 'K Means', kmeans.labels_)
