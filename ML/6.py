f32c17256d1ba373fb23bac800f4901968c642bd
rednithin <reddy.nithinpg@live.com>
2018-11-29 02:29:22 +0530

    Modified 6th program

--- ML/6.py
+++ ML/6.py
@@ -1,8 +1,7 @@
 from sklearn.datasets import fetch_20newsgroups
-from sklearn.metrics import confusion_matrix, accuracy_score, classification_report
-from sklearn.naive_bayes import MultinomialNB
+from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
 from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
-from pprint import pprint
+from sklearn.naive_bayes import MultinomialNB
 
 categories = ['alt.atheism', 'soc.religion.christian',
               'comp.graphics', 'sci.med']
@@ -12,18 +11,14 @@ test = fetch_20newsgroups(subset='test', categories=categories)
 
 c, t = CountVectorizer(), TfidfTransformer()
 
-train_tr = t.fit_transform(c.fit_transform(train.data))
+trainTr = t.fit_transform(c.fit_transform(train.data))
+testTr = t.transform(c.transform(test.data))
 
 model = MultinomialNB()
-model.fit(train_tr, train.target)
+model.fit(trainTr, train.target)
+predicted = model.predict(testTr)
 
-test_tr = t.transform(c.transform(test.data))
-predicted = model.predict(test_tr)
-
-print("Accuracy:", accuracy_score(test.target, predicted))
-
-print("Classification Report:")
-print(classification_report(test.target, predicted, target_names=test.target_names))
-
-print("Confusion Matrix:")
-pprint(confusion_matrix(test.target, predicted))
+print('Accuracy : ', accuracy_score(test.target, predicted), sep='\n')
+print('Classification Report : ', classification_report(
+    test.target, predicted, target_names=test.target_names), sep='\n')
+print('Confusion Matrix : ', confusion_matrix(test.target, predicted), sep='\n')
