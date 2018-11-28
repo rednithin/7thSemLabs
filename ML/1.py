594daa71abc4a5754a93966e71919bd44d414106
rednithin <reddy.nithinpg@live.com>
2018-11-29 01:10:29 +0530

    Fixed error in 1st

--- ML/1.py
+++ ML/1.py
@@ -2,10 +2,10 @@ from csv import reader
 from pprint import pprint
 
 with open("1-dataset.csv") as f:
-    dataset = [row[:-1] for row in reader(f) if row[-1] == "yes"]
+    dataset = [row[:-1] for row in reader(f) if row[-1] == "Yes"]
 
 hypothesis = dataset[0][:]
 for row in dataset[1:]:
     hypothesis = ["?" if tup[0] != tup[1] else tup[0]
-                  for tup in zip(hypothesis, row[:-1])]
+                  for tup in zip(hypothesis, row)]
 print(hypothesis)
