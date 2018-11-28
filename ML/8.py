cbca40bfdf3ab4c888882d30bf99aaef26791eb1
rednithin <reddy.nithinpg@live.com>
2018-11-29 02:35:16 +0530

    Shortened 8th code.

--- ML/8.py
+++ ML/8.py
@@ -1,25 +1,20 @@
-from sklearn.mixture import GaussianMixture
-import matplotlib.pyplot as plt
-from sklearn import datasets
-from sklearn.cluster import KMeans
-import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.datasets import load_iris
+from sklearn.cluster import KMeans
+from sklearn.mixture import GaussianMixture
 
 
 plt.figure(figsize=(14, 14)).subplots_adjust(hspace=0.4, wspace=0.4)
 colormap = np.array(['red', 'lime', 'black'])
 
-iris = datasets.load_iris()
-X = pd.DataFrame(iris.data)
-X.columns = ['Sepal_Length', 'Sepal_Width', 'Petal_Length', 'Petal_Width']
-
-Y = pd.DataFrame(iris.target)
-Y.columns = ['Targets']
+iris = load_iris()
+X, Y = iris.data, iris.target
 
 
 def plot(i, title, targets):
     plt.subplot(2, 2, i)
-    plt.scatter(X.Petal_Length, X.Petal_Width, c=colormap[targets])
+    plt.scatter(X[:, 2], X[:, 3], c=colormap[targets])
     plt.title(title)
     plt.xlabel('Petal_Length')
     plt.ylabel('Petal_Width')
@@ -32,8 +27,8 @@ gmm = GaussianMixture(n_components=3)
 gmm.fit(X)
 gmm_y = gmm.predict(X)
 
-plot(1, 'Real', Y.Targets)
+plot(1, 'Real', Y)
 plot(2, 'K Means', kmeans.labels_)
-plot(3, 'Real', Y.Targets)
+plot(3, 'Real', Y)
 plot(4, 'GMM', gmm_y)
 plt.show()
