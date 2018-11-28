dfa66fd2f5991187fa771b8d6ecd050d7c9eaa40
rednithin <reddy.nithinpg@live.com>
2018-11-29 03:01:02 +0530

    Modified 8th one.

--- ML/8.py
+++ ML/8.py
@@ -1,36 +1,36 @@
 import numpy as np
-import matplotlib.pyplot as plt
-from sklearn.datasets import load_iris
 from sklearn.cluster import KMeans
 from sklearn.mixture import GaussianMixture
+from sklearn.datasets import load_iris
 from sklearn.metrics import silhouette_score
+import matplotlib.pyplot as plt
 
 colormap = np.array(['red', 'lime', 'black'])
-
 iris = load_iris()
 X, Y = iris.data, iris.target
-
-
-def plot(i, title, targets):
-    plt.subplot(2, 2, i)
-    plt.title(title)
-    plt.scatter(X[:, 2], X[:, 3], c=colormap[targets])
-    plt.xlabel('Petal Length')
-    plt.ylabel('Petal Width')
-
 
 kmeans = KMeans(n_clusters=3)
 kmeans.fit(X)
 
 gmm = GaussianMixture(n_components=3)
 gmm.fit(X)
-gmm_y = gmm.predict(X)
+gmmY = gmm.predict(X)
 
-print('Kmeans: ', silhouette_score(X, kmeans.labels_))
-print('GMM: ', silhouette_score(X, gmm_y))
+print('Kmeans', silhouette_score(X, kmeans.labels_))
+print('GMM', silhouette_score(X, gmmY))
+
+
+def plot(i, title, targets):
+    plt.subplot(2, 2, i)
+    plt.scatter(X[:, 2], X[:, 3], c=colormap[targets])
+    plt.title(title)
+    plt.xlabel('Petal Length')
+    plt.ylabel('Petal Width')
+
 
 plot(1, 'Real', Y)
 plot(2, 'K Means', kmeans.labels_)
 plot(3, 'Real', Y)
-plot(4, 'GMM', gmm_y)
+plot(4, 'GMM', gmmY)
+
 plt.show()
